23746ab851212257e623435fe864bc51392f3023
CanFam23 <nickclouse03@gmail.com>
2024-09-06 10:23:32 -0600

    Initial commit

---
+++ src/main/java/edu/carroll/initMusic/InitMusicApplication.java
@@ -0,0 +1,13 @@
+package edu.carroll.initMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/carroll/initMusic/InitMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.carroll.initMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
