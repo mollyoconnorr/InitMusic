a608cd9c21968c2b40e8559f037b9b86cd7d045f
Molly O'Connor <moconnor@carroll.edu>
2024-09-09 11:44:08 -0600

    Added basic login page

--- src/main/java/edu/carroll/initMusic/MainController.java
+++ src/main/java/edu/carroll/initMusic/MainController.java
@@ -2,11 +2,12 @@ package edu.carroll.initMusic;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class MainController {
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "index";
     }
 }
