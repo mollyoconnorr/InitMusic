044f637af530792b0cf34cdd8a599087db39bbdc
CanFam23 <nickclouse03@gmail.com>
2024-10-09 14:55:02 -0600

    Added playlist name to deletePlaylist modal

--- src/main/resources/static/js/playlistPage.js
+++ src/main/resources/static/js/playlistPage.js
@@ -8,10 +8,22 @@ function populateRenamePlaylistModal(modal, playlistName) {
     }
 }
 
+function populateDeletePlaylistModal(modal, playlistName){
+    const modalBody = modal.querySelector('.modal-body');
+    modalBody.textContent = 'Are you sure you want to delete ' + playlistName + '?';
+}
+
 // Hook into the modal's 'show.bs.modal' event
 const renamePlaylistModal = document.getElementById('renamePlaylistModal');
 renamePlaylistModal.addEventListener('show.bs.modal', function (event) {
     const button = event.relatedTarget;
     const playlistName = button.getAttribute('data-playlist-name'); // Get the playlist name
     populateRenamePlaylistModal(renamePlaylistModal, playlistName); // Pass it to the modal
+});
+
+const deletePlaylistModal = document.getElementById('deletePlaylistModal');
+deletePlaylistModal.addEventListener('show.bs.modal',function (event){
+   const button = event.relatedTarget;
+   const playlistName = button.getAttribute('data-playlist-name');
+   populateDeletePlaylistModal(deletePlaylistModal,playlistName);
 });
