2265629fc30092f00b836f05e0b40c7000cdefa4
CanFam23 <nickclouse03@gmail.com>
2024-10-17 15:43:45 -0600

    Added enum for response status, used to provide user feedback

---
+++ src/main/java/edu/carroll/initMusic/ResponseStatus.java
@@ -0,0 +1,25 @@
+package edu.carroll.initMusic;
+
+public enum ResponseStatus {
+    SUCCESS("Success"),
+    USER_NOT_FOUND("User not found!"),
+    SONG_NOT_FOUND("Song not found!"),
+    PLAYLIST_NOT_FOUND("Playlist not found!"),
+    PLAYLIST_ALREADY_EXISTS("Playlist already exists!"),
+    PLAYLIST_NAME_EXISTS("Playlist with given name already exists!"),
+    PLAYLIST_NAME_EMPTY("Playlist name cannot be empty!");
+
+    private final String message;
+
+    ResponseStatus(String message) {
+        this.message = message;
+    }
+
+    public boolean passed(){
+        return SUCCESS.getMessage().equals(this.message);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
