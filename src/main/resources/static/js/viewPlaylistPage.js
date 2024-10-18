6ef0877beb3dbed02b6ee196aa97023d1d70fc96
CanFam23 <nickclouse03@gmail.com>
2024-10-17 21:44:47 -0600

    Added js to populate modal and form with data for viewPlaylist page

---
+++ src/main/resources/static/js/viewPlaylistPage.js
@@ -0,0 +1,19 @@
+function populateDeleteSongModal(modal, songID, songName, playlistID, playlistName){
+    const modalBody = modal.querySelector('.modal-body');
+    modalBody.textContent = 'Are you sure you want to delete ' + songName + ' from ' +
+    playlistName + '?';
+
+    document.getElementById('playlistID').value = playlistID;
+    document.getElementById('songID').value = songID;
+}
+
+const deleteSongModal = document.getElementById('deleteSongModal');
+deleteSongModal.addEventListener('show.bs.modal', function (event) {
+    const button = event.relatedTarget;
+    const songID = button.getAttribute('data-song-id');
+    const songName = button.getAttribute('data-song-name')
+    const playlistID = button.getAttribute('data-playlist-id');
+    const playlistName = button.getAttribute('data-playlist-name')
+
+    populateDeleteSongModal(deleteSongModal, songID, songName, playlistID, playlistName);
+});
