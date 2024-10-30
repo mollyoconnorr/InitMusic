ea4cdd2e3fcefc9a1a0ddd5897e4ef49bab1f31d
CanFam23 <nickclouse03@gmail.com>
2024-10-30 11:42:26 -0600

    Updated CustomerUserDetailsService

--- src/main/java/edu/carroll/initMusic/service/CustomUserDetailsService.java
+++ src/main/java/edu/carroll/initMusic/service/CustomUserDetailsService.java
@@ -3,6 +3,8 @@ package edu.carroll.initMusic.service;
 import edu.carroll.initMusic.config.CustomUserDetails;
 import edu.carroll.initMusic.jpa.model.User;
 import edu.carroll.initMusic.jpa.repo.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,6 +14,8 @@ import java.util.List;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+    private static final Logger log = LoggerFactory.getLogger(CustomUserDetailsService.class);
+
 
     private final UserRepository userRepository;
 
@@ -21,6 +25,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("loadUserByUsername: username={}", username);
         final List<User> user = userRepository.findByUsernameIgnoreCase(username);
         if(user.size() != 1) {
             throw new UsernameNotFoundException("User not found");
