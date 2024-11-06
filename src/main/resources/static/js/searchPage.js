1506e14cc625bc136865ee82513e75d4e1eac4b7
CanFam23 <nickclouse03@gmail.com>
2024-11-06 15:23:57 -0700

    Added custom error page,styled it,moved logout logic to own js file

--- src/main/resources/static/js/searchPage.js
+++ src/main/resources/static/js/searchPage.js
@@ -29,36 +29,6 @@ function populateModal(button) {
 
 }
 
-/**
- * Shows a popup message that makes user confirm logout, and then fetches post for /logout and logs user out
- */
-function logout() {
-    // Show confirmation dialog
-    const userConfirmed = confirm("Are you sure you want to log out?");
-
-    // log out only if the user confirms
-    if (userConfirmed) {
-        fetch('/logout', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Requested-With': 'XMLHttpRequest' // CSRF protection
-            },
-            credentials: 'include' // include cookies in the request
-        })
-            .then(response => {
-                if (response.ok) {
-                    // Redirect to the login page
-                    window.location.href = '/login?logout';
-                } else {
-                    throw new Error('Logout failed');
-                }
-            })
-            .catch(error => {
-                console.error('Error during logout:', error);
-            });
-    }
-}
 
 let popupWindow = null; // Global variable to store the pop-up window reference
 
@@ -104,6 +74,3 @@ document.addEventListener('click', function(event) {
         closePopup(); // Close the pop-up if it's open and the click is outside of the links
     }
 });
-
-//add an event listener to the button
-document.getElementById('logoutButton').addEventListener('click', logout);
