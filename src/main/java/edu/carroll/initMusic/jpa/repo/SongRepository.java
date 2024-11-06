e0fa3ecd5cf124a6a3931cb2962772aea97ce1ee
CanFam23 <nickclouse03@gmail.com>
2024-11-05 20:16:56 -0700

    Removed two methods from SongRepo

--- src/main/java/edu/carroll/initMusic/jpa/repo/SongRepository.java
+++ src/main/java/edu/carroll/initMusic/jpa/repo/SongRepository.java
@@ -2,8 +2,6 @@ package edu.carroll.initMusic.jpa.repo;
 
 import edu.carroll.initMusic.jpa.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -21,27 +19,4 @@ public interface SongRepository extends JpaRepository<Song, Long> {
      * @return List of songs found with name or similar names
      */
     List<Song> findBySongNameContainingIgnoreCaseOrderBySongName(String name);
-
-    /**
-     * Finds songs by song name and artist name
-     * @param songName Song name to search for
-     * @param artistName Artist name to search for
-     * @return
-     */
-    List<Song> findBySongNameAndArtists_ArtistName(String songName, String artistName);
-
-    /**
-     * Finds songs by artist
-     * @param name Artist's name
-     * @return List of songs found
-     */
-    List<Song> findAllByArtists_ArtistName(String name);
-
-    /**
-     * Finds songs by album
-     * @param albumTitle Album title to search for
-     * @return List of songs found
-     */
-    @Query("SELECT s FROM Song s JOIN s.album a WHERE LOWER(a.albumName) = LOWER(:albumTitle)")
-    List<Song> findByAlbumTitleIgnoreCase(@Param("albumTitle") String albumTitle);
 }
