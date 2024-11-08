8297184d7ccd954f84b0527f196454e14df0e818
Molly O'Connor <moconnor@carroll.edu>
2024-11-07 17:20:49 -0700

    Added js for the login and delete account confirmation

---
+++ src/main/resources/static/js/myProfile.js
@@ -0,0 +1,38 @@
+function confirmDelete() {
+    return confirm("Are you sure you want to delete your account? This action cannot be undone.");
+}
+
+function confirmLogout() {
+    return confirm("Are you sure you want to logout?");
+}
+
+/**
+ * Shows a popup message that makes user confirm logout, and then fetches post for /logout and logs user out
+ */
+function logout() {
+    // Show confirmation dialog
+    const userConfirmed = confirm("Are you sure you want to log out?");
+
+    // log out only if the user confirms
+    if (userConfirmed) {
+        fetch('/logout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest' // CSRF protection
+            },
+            credentials: 'include' // include cookies in the request
+        })
+            .then(response => {
+                if (response.ok) {
+                    // Redirect to the login page
+                    window.location.href = '/login?logout';
+                } else {
+                    throw new Error('Logout failed');
+                }
+            })
+            .catch(error => {
+                console.error('Error during logout:', error);
+            });
+    }
+}
