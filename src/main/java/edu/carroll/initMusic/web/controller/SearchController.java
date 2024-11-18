5486163b86aa5eccb2368eb1477d9e5fb02f77e4
CanFam23 <nickclouse03@gmail.com>
2024-11-18 15:55:28 -0700

    Removed unused code in search template and controller, fixed 2 bugs

--- src/main/java/edu/carroll/initMusic/web/controller/SearchController.java
+++ src/main/java/edu/carroll/initMusic/web/controller/SearchController.java
@@ -44,7 +44,7 @@ public class SearchController {
     /** Logger for logging */
     private static final Logger log = LoggerFactory.getLogger(SearchController.class);
 
-    /** Song service for operations */
+    /** Song service for operations with songs and caches*/
     private final SongService songService;
 
     /** User service for operations with user objects */
@@ -105,12 +105,6 @@ public class SearchController {
             model.addAttribute("query", null);
         }
 
-        if(session.getAttribute("localQuery") != null) {
-            model.addAttribute("localQuery", session.getAttribute("localQuery"));
-        }else{
-            model.addAttribute("localQuery", false);
-        }
-
         model.addAttribute("newSongForm", new NewSongForm());
         model.addAttribute("NewPlaylistForm", new NewPlaylistForm());
 
@@ -136,127 +130,26 @@ public class SearchController {
 
         //If query doesnt have any text
         if (query.trim().isEmpty() || query.length() < 3) {
+            //For whatever reason, when this if statement is triggered, the model is missing the
+            //new playlist form, so we have to add it again here
+            model.addAttribute("NewPlaylistForm", new NewPlaylistForm());
             model.addAttribute("searchError", "Search term must be at least 3 characters long.");
             return "search"; // Return to the search page with error message
         }
 
-        Set<Song> results = songService.getLocalCache(query);
-
-        //If no cache found, then do global search
-        if(results == null){
-            results = songService.searchForSongs(query);
-        }else{
-            //Local cache found, set these to true so message displays on search page that search was local
-            model.addAttribute("localQuery", true);
-            session.setAttribute("localQuery",true);
-        }
-
-        //If no songs found
-        if(results.isEmpty()) {
-            model.addAttribute("searchError", String.format("No songs found for query '%s'", query));
-            return "search";
-        }
-
-        session.setAttribute("results", results);
-        session.setAttribute("query", query);
-        model.addAttribute("results", results);
-        model.addAttribute("query", query);
-        model.addAttribute("currentUser", user);
-        model.addAttribute("playlists",user.getPlaylists());
-        model.addAttribute("newSongForm", new NewSongForm());
-        model.addAttribute("NewPlaylistForm", new NewPlaylistForm());
-
-        return "search";
-    }
-
-    /**
-     * Gets the /searchWithAPI endpoint, which is after a user
-     * searches uses an external API
-     * @param model Model to use
-     * @param authentication Current authenticated user token, if any
-     * @return Search page
-     */
-    @GetMapping("/searchWithAPI")
-    public String getSearchWithAPI(Model model, Authentication authentication,HttpSession session){
-        //Retrieve the current user from the session
-        final CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-        final User user = userDetails.getUser();
-
-        //Fetch user with playlists
-        final User fullUser = userService.findByIdWithPlaylists(user.getuserID());
-
-        log.info("getSearchWithAPI: {} went to searchWithAPI endpoint", fullUser.getuserID());
-
-        //Add the user and their playlists to the model
-        model.addAttribute("currentUser", fullUser);
-        model.addAttribute("playlists", fullUser.getPlaylists());
-
-        /*
-          Result is stored in the httpSession so its available after adding song to a playlist.
-          When a user first goes to search page, there is no results yet, so set it to a empty hashset
-         */
-        if(session.getAttribute("results") instanceof Set<?> && session.getAttribute("results") != null) {
-            model.addAttribute("results", session.getAttribute("results"));
-        }else{
-            model.addAttribute("results", new HashSet<>());
-        }
-
-        /*
-          query is stored in the httpSession so its available after adding song to a playlist.
-          When a user first goes to search page, there is no results yet, so set it to a new song form
-         */
-        if(session.getAttribute("query") != null) {
-            model.addAttribute("query", session.getAttribute("query"));
-        }else{
-            model.addAttribute("query", null);
-        }
-
-        if(session.getAttribute("localQuery") != null) {
-            model.addAttribute("localQuery", session.getAttribute("localQuery"));
-        }else{
-            model.addAttribute("localQuery", false);
-        }
-
-        model.addAttribute("newSongForm", new NewSongForm());
-        model.addAttribute("NewPlaylistForm", new NewPlaylistForm());
-
-        return "search"; // Return to the search page
-    }
-
-    /**
-     * Searches with the 'searchForSongs' method in SongService, which should search using an
-     * api and return a set of songs found related to query
-     * @param query String to search for
-     * @param model Model to use
-     * @param authentication Current authenticated user token, if any
-     * @return Updated search page
-     *
-     * @see SongService#searchForSongs(String)
-     */
-    @PostMapping("/searchWithAPI")
-    public String searchWithAPI(@RequestParam(value = "query") String query, Model model, Authentication authentication, HttpSession session){
-        //Reload user details
-        final CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-        final User user = userService.findByIdWithPlaylists(userDetails.getUser().getuserID());
-
-        //Search for songs
         final Set<Song> results = songService.searchForSongs(query);
 
         //If no songs found
         if(results.isEmpty()) {
+            //For whatever reason, when this if statement is triggered, the model is missing the
+            //new playlist form, so we have to add it again here
+            model.addAttribute("NewPlaylistForm", new NewPlaylistForm());
             model.addAttribute("searchError", String.format("No songs found for query '%s'", query));
             return "search";
         }
 
-        //Add query, results to session
         session.setAttribute("results", results);
         session.setAttribute("query", query);
-
-        //Set local query to false bc this was not local search
-        session.setAttribute("localQuery",false);
-        model.addAttribute("localQuery",false);
-
-        //Reload model attributes
         model.addAttribute("results", results);
         model.addAttribute("query", query);
         model.addAttribute("currentUser", user);
